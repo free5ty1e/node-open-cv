d2b64fd2fc3614a145d5bf0d458718a54d13e8e5
Chris Paiano <chris.paiano@nike.com>
2015-02-08 20:39:44 -0800

    -Added openCV jar and lib and imported them to node, now able to actually import the SimpleSample class without crashing.   (main crashes)

--- node/nodeOpenCvTest.js
+++ node/nodeOpenCvTest.js
@@ -2,8 +2,9 @@ var java = require("java");
 java.classpath.push("commons-lang3-3.1.jar");
 java.classpath.push("commons-io.jar");
 java.classpath.push("SimpleSample.jar");
+java.classpath.push("opencv-2410.jar");
+java.classpath.push("libopencv_java2410.dylib");
 var SimpleSample = java.import("SimpleSample");
-var simpleSample = java.newInstanceSync("SimpleSample");
-SimpleSample.main(null);
+SimpleSample.main("");
 
 console.log("Appears to be handling the above OpenCV Java correctly...");
