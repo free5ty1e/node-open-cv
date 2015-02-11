ede7ce411baa29148d225db6dbaf703736a2315d
Chris Paiano <chris.paiano@nike.com>
2015-02-10 22:26:49 -0800

    -Split dependencies into two stage install to prevent broken packages, also added more required dependencies to build openCV

--- scripts/buildOpenCvOnRaspberryPi.sh
+++ scripts/buildOpenCvOnRaspberryPi.sh
@@ -8,7 +8,9 @@ printf "CONF_SWAPSIZE=1024\n" >> ~/swapspec.txt
 sudo cp ~/swapspec.txt /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
-sudo apt-get update && sudo apt-get install -y oracle-java7-jdk ant cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev gcc-4.7
+sudo apt-get -y update
+sudo apt-get -y install build-essential cmake cmake-qt-gui pkg-config libpng12-0 libpng12-dev libpng++-dev libpng3 libpnglite-dev zlib1g-dbg zlib1g zlib1g-dev pngtools libtiff4-dev libtiff4 libtiffxx0c2 libtiff-tools oracle-java7-jdk ant git libjpeg-dev libpng-dev libtiff-dev libjasper-dev gcc-4.7
+sudo apt-get -y install libjpeg8 libjpeg8-dev libjpeg8-dbg libjpeg-progs ffmpeg libavcodec-dev libavcodec53 libavformat53 libavformat-dev libgstreamer0.10-0-dbg libgstreamer0.10-0 libgstreamer0.10-dev libxine1-ffmpeg libxine-dev libxine1-bin libunicap2 libunicap2-dev libdc1394-22-dev libdc1394-22 libdc1394-utils swig libv4l-0 libv4l-dev python-numpy libpython2.6 python-dev python2.6-dev libgtk2.0-dev pkg-config libswscale-dev
 #libtbb-dev libtbb2
 git clone git://github.com/Itseez/opencv.git
 cd opencv
