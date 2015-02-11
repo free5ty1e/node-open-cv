69f090562afa517e54f2b4361e5957cb2d183dae
Chris Paiano <chris.paiano@nike.com>
2015-02-10 22:51:35 -0800

    -Added npm config and more echo detail, also now node and npm included in apt install stage

--- scripts/buildOpenCvOnRaspberryPi.sh
+++ scripts/buildOpenCvOnRaspberryPi.sh
@@ -9,16 +9,28 @@ sudo cp ~/swapspec.txt /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
 sudo apt-get -y update
-sudo apt-get -y install build-essential cmake cmake-qt-gui pkg-config libpng12-0 libpng12-dev libpng++-dev libpng3 libpnglite-dev zlib1g-dbg zlib1g zlib1g-dev pngtools libtiff4-dev libtiff4 libtiffxx0c2 libtiff-tools oracle-java7-jdk ant git libjpeg-dev libpng-dev libtiff-dev libjasper-dev gcc-4.7
+sudo apt-get -y install build-essential cmake cmake-qt-gui pkg-config libpng12-0 libpng12-dev libpng++-dev libpng3 libpnglite-dev zlib1g-dbg zlib1g zlib1g-dev pngtools libtiff4-dev libtiff4 libtiffxx0c2 libtiff-tools oracle-java7-jdk ant git gcc-4.7 npm node libjpeg-dev libpng-dev libtiff-dev libjasper-dev
 sudo apt-get -y install libjpeg8 libjpeg8-dev libjpeg8-dbg libjpeg-progs ffmpeg libavcodec-dev libavcodec53 libavformat53 libavformat-dev libgstreamer0.10-0-dbg libgstreamer0.10-0 libgstreamer0.10-dev libxine1-ffmpeg libxine-dev libxine1-bin libunicap2 libunicap2-dev libdc1394-22-dev libdc1394-22 libdc1394-utils swig libv4l-0 libv4l-dev python-numpy libpython2.6 python-dev python2.6-dev libgtk2.0-dev pkg-config libswscale-dev
 #libtbb-dev libtbb2
+echo Configuring NPM stuffs...
+sudo npm config set strict-ssl false
+npm config set strict-ssl false
+sudo npm update
+npm update
+sudo npm install -g node-gyp
+GYP_DEFINES="armv7=0" CCFLAGS='-march=armv6' CXXFLAGS='-march=armv6' npm install java
+
+echo Cloning OpenCV git repository...
 git clone git://github.com/Itseez/opencv.git
 cd opencv
 git checkout 2.4
 mkdir build
 cd build
+echo Configuring OpenCV build...
 #cmake -DBUILD_SHARED_LIBS=OFF ..
 export JAVA_HOME=/usr/lib/jvm/jdk-7-oracle-armhf
 CC=gcc-4.7 CXX=g++-4.7 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON -D BUILD_EXAMPLES=ON -DBUILD_SHARED_LIBS=OFF ..
+echo Compiling OpenCV...
 make -j8
+echo Hooray, if no errors are shown above then you might have OpenCV on your Raspberry Pi!
 popd
